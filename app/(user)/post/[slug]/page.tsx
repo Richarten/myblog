1aa45ad113c299e9f56f299c06e269f1b65d5f04
Nick <nick.richarte@gmail.com>
2023-03-21 18:35:46 -0500

    article content page finished

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -21,35 +21,65 @@ async function Post({ params: { slug } }: Props) {
 
 	const post: Post = await client.fetch(query, { slug });
 
-	return;
-	<article className="px-10 pb-28">
-		<section className="spaye-y-2 border border-[#E3735E] text-white">
-			<div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-				<div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
-					<Image
-						className="object-cover object-center mx-auto"
-						src={urlFor(post.mainImage).url()}
-						alt={post.author.name}
-						fill
-					/>
-				</div>
-				<section>
-					<div>
-						<div>
-							<h1 className="text-4xl font-extrabold">{post && post.title}</h1>
-							<p>
-								{new Date(post._createdAt).toLocaleDateString("en-US", {
-									day: "numeric",
-									month: "long",
-									year: "numeric",
-								})}
-							</p>
-						</div>
+	return (
+		<article className="px-10 pb-28">
+			<section className="spaye-y-2 border border-[#E3735E] text-white">
+				<div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+					<div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+						<Image
+							className="object-cover object-center mx-auto"
+							src={urlFor(post.mainImage).url()}
+							alt={post.author.name}
+							fill
+						/>
 					</div>
-				</section>
-			</div>
-		</section>
-	</article>;
+					<section className="p-5 bg-[#E3735E] w-full">
+						<div className="flex flex-col md:flex-row justify-between gap-y-5">
+							<div>
+								<h1 className="text-4xl font-extrabold">
+									{post && post.title}
+								</h1>
+								<p>
+									{new Date(post._createdAt).toLocaleDateString("en-US", {
+										day: "numeric",
+										month: "long",
+										year: "numeric",
+									})}
+								</p>
+							</div>
+							<div className="flex items-center space-x-2">
+								<Image
+									className="rounded-full"
+									src={urlFor(post.author.image).url()}
+									alt={post.author.name}
+									height={40}
+									width={40}
+								/>
+								<div className="w-64">
+									<h3 className="text-lg font-bold">{post.author.name}</h3>
+									<div>{/*TODO: Author Bio*/}</div>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<h2 className="italic pt-10">{post.description}</h2>
+							<div className="flex items-center justify-end mt-auto space-x-2">
+								{post.categories.map((category) => (
+									<p
+										key={category._id}
+										className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+									>
+										{category.title}
+									</p>
+								))}
+							</div>
+						</div>
+					</section>
+				</div>
+			</section>
+		</article>
+	);
 }
 
 export default Post;
